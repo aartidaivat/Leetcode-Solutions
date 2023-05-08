a28f97059f984f1820d2e324e7d33a9ec0d13d61
aartidaivat <126923111+aartidaivat@users.noreply.github.com>
2023-05-08 09:43:08 -0700

    Update 202. Happy Number.java

--- 202. Happy Number.java
+++ 202. Happy Number.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isHappy(int n) {  
-        int temp=0;
-        while(n!=0) {
-            int x=n%10;
-            n=n/10;
-            temp += x*x;
-        }
-        n=temp;
-        if(n<10) {
-            if(n==1 || n==7) {
-                return true;
-            } 
-            return false;   
-        }
-        return isHappy(n);
+        Set<Integer> set = new HashSet<>();
+        while(n!=1) {
+            int temp=0;
+            while(n!=0) {
+                int x=n%10;
+                n=n/10;
+                temp += x*x;
+            }
+            n=temp;
+            if(set.contains(n)) {
+                return false;
+            }
+            set.add(n);
+        }  
+        return true;
     }
 }
+
