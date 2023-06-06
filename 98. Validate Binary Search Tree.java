8c693542991b2c12402a437495192c9f4bf0321b
aartidaivat <126923111+aartidaivat@users.noreply.github.com>
2023-06-06 14:59:13 -0700

    Update 98. Validate Binary Search Tree.java

--- 98. Validate Binary Search Tree.java
+++ 98. Validate Binary Search Tree.java
@@ -16,13 +16,13 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root==null)  return true;
-        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return validBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-  
-    public boolean dfs(TreeNode root, long min, long max) {
+
+    public boolean validBST(TreeNode root, long min, long max) {
         if(root==null) return true;
         if(min<root.val && root.val<max) {
-            return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+            return validBST(root.left, min, root.val) && validBST(root.right, root.val, max);
         }
         return false;
     }
